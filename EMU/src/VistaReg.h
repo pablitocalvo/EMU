18cc8035b3e79fba1aee969b1d2cb561c38afd3d
pablitocalvo <foo@foo.foo>
2019-02-26 12:54:27 +0100

    rimosso stato dalle viste

--- EMU/src/VistaReg.h
+++ EMU/src/VistaReg.h
@@ -21,7 +21,8 @@ class VistaReg : Vista
 {public:
 
 	VistaReg(  Registro & r ): reg(r)
-	{ stato ='-';
+	{
+		//stato ='-';
 	reg.reg_is_read.connect( sigc::bind( sigc::mem_fun(this, &VistaReg::on_reg_is_read ), &reg ));
 	reg.reg_is_write.connect( sigc::bind( sigc::mem_fun(this, &VistaReg::on_reg_is_write ), &reg ));
 
@@ -29,26 +30,26 @@ class VistaReg : Vista
 
 	void on_reg_is_read(Registro * R)
 	{
-		stato='R';
+		//stato='R';
 		cout<<vedi()<<endl;
 
 
 	};
 
 	void on_reg_is_write(Registro * R)
-	{	stato='W';
+	{	//stato='W';
 		cout<<vedi()<<endl;
 	};
 
 
 	string vedi()
 	{
-		return ( reg.nome + " = " + to_string( (int) reg.getValore()) + stato);
+		return ( reg.nome + " = " + to_string( (int) reg.getValore()) + reg.stato);
 	}
 
 private:
 	Registro  & reg;
-	char stato ; // r w -;
+	//char stato ; // r w -;
 
 };
 #endif /* VISTAREG_H_ */
