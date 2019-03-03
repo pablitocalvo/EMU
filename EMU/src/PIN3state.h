cb2c8c1074269b1fd6cc0697a238997be74f41d2
pablitocalvo <foo@foo.foo>
2019-03-03 14:40:37 +0100

    bo

---
+++ EMU/src/PIN3state.h
@@ -0,0 +1,26 @@
+/*
+ * PIN3state.h
+ *
+ *  Created on: 03/mar/2019
+ *      Author: paolo
+ */
+
+#ifndef PIN3STATE_H_
+#define PIN3STATE_H_
+
+
+class PIN_3state
+{
+public:
+    PIN_3state();
+    virtual
+    ~PIN_3state();
+
+valore
+
+
+
+
+};
+
+#endif /* PIN3STATE_H_ */
