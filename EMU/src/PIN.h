31a22519775df7770e94ba333a2f98b407054d19
pablitocalvo <foo@foo.foo>
2019-03-04 21:07:56 +0100

    intoroddti signal ....

--- EMU/src/PIN.h
+++ EMU/src/PIN.h
@@ -8,6 +8,7 @@
 #ifndef PIN_H_
 #define PIN_H_
 
+#include <sigc++/sigc++.h>
 #include "common.h"
 class PIN
 {
@@ -17,15 +18,19 @@ class PIN
         virtual
         ~PIN(){}
 
+        sigc::signal<void> pin_writed_to_LOW;
+        sigc::signal<void> pin_writed_to_HIGH;
+        sigc::signal<void> pin_toggled;
+
     private:
         Pin_3state_value valore ;
         string nome;
 
     public:
 
-        void set_high(){valore=PIN_HIGH;}
-        void set_low() {valore=PIN_LOW;}
-        void toggle() { if (valore==PIN_HIGH)  set_low(); else set_high() ;}
+        void set_high(){valore=PIN_HIGH;pin_writed_to_HIGH();}
+        void set_low() {valore=PIN_LOW;pin_writed_to_LOW();}
+        void toggle() { if (valore==PIN_HIGH)  set_low(); else set_high() ;pin_writed_to_LOW();}
 
         Pin_3state_value
         get_value()
