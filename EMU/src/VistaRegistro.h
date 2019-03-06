52be892232dc2a6fd13433350498a136eb6b7ffa
pablitocalvo <foo@foo.foo>
2019-03-06 19:13:24 +0100

    primo_commit

---
+++ EMU/src/VistaRegistro.h
@@ -0,0 +1,22 @@
+/*
+ * VistaRegistro.h
+ *
+ *  Created on: 06/mar/2019
+ *      Author: paolo
+ */
+
+#ifndef VISTAREGISTRO_H_
+#define VISTAREGISTRO_H_
+
+class VistaRegistro : public Vista
+{
+public:
+    VistaRegistro();
+    virtual
+    ~VistaRegistro();
+
+
+
+};
+
+#endif /* VISTAREGISTRO_H_ */
