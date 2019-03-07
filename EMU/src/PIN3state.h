fc9d89eca9d866c29d5c8953ae119ebc6a62ade3
pablitocalvo <foo@foo.foo>
2019-03-07 10:53:12 +0100

    agg.to signal , mod toglge()

--- EMU/src/PIN3state.h
+++ EMU/src/PIN3state.h
@@ -8,8 +8,6 @@
 #ifndef PIN3STATE_H_
 #define PIN3STATE_H_
 #include "common.h"
-
-
 class PIN_3state
 {
     public:
@@ -18,19 +16,38 @@ class PIN_3state
         virtual
         ~PIN_3state(){};
 
+        sigc::signal<void> pin_writed_to_LOW;
+        sigc::signal<void> pin_writed_to_HIGH;
+        sigc::signal<void> pin_toggled;
+        sigc::signal<void> pin_enabled;
+        sigc::signal<void> pin_disabled;
+
+
     private:
         Pin_3state_value valore,en,output ;
         string nome;
+
+
     public:
 
-        void enable() { en=PIN_HIGH; output=valore;}
-        void disable() { en=PIN_LOW;output=PIN_Z;}
+        void enable()  { en=PIN_HIGH; output=valore; pin_enabled(); }
+        void disable() { en=PIN_LOW ; output=PIN_Z ; pin_disabled();}
 
-        void set_high(){valore=PIN_HIGH;}
-        void set_low() {valore=PIN_LOW;}
+        void set_high(){ valore=PIN_HIGH; pin_writed_to_HIGH(); }
+        void set_low() { valore=PIN_LOW;  pin_writed_to_LOW();  }
+        void toggle()  { if (valore==PIN_HIGH)
+                                      {set_low()  ;  pin_writed_to_LOW();}
+                                else  {set_high() ;  pin_writed_to_LOW();}
+                                pin_toggled();
+                        }
 
         Pin_3state_value
         get_value(){  return ( (en==PIN_HIGH) ? valore : PIN_Z);  }
+
+        string
+        get_nome()
+        { return nome;}
+
 };
 
 #endif /* PIN3STATE_H_ */
